193944435101e640cd67f914be1589a1f82ba284
Samuel Theising <theisingsamuel@gmail.com>
2025-06-23 19:55:31 -0400

    Services a bit better

--- js/map.js
+++ js/map.js
@@ -1,7 +1,14 @@
++9-3
 document.addEventListener("DOMContentLoaded", function () {
-  const map = L.map('map').setView([39.7684, -86.1581], 13);
+  const map = L.map('map').setView([39.7684, -86.1581], 6);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+
+  L.circle([39.7684, -86.1581], {
+    radius: 321869,
+    color: 'blue',
+    fillOpacity: 0.1
   }).addTo(map);
 });
